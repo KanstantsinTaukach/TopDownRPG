e0c7cfa483a2da28fdb013b544ab4c86ed6ee06e
Kot <Tovkachkot@gmail.com>
2025-09-23 12:32:12 +0300

    Character Blueprint Setup

--- Source/TopDownRPG/Private/Player/TDRPGCharacterBase.cpp
+++ Source/TopDownRPG/Private/Player/TDRPGCharacterBase.cpp
@@ -5,6 +5,10 @@
 ATDRPGCharacterBase::ATDRPGCharacterBase()
 {
     PrimaryActorTick.bCanEverTick = false;
+
+    WeaponSkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>("WeaponSkeletalMesh");
+    WeaponSkeletalMesh->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+    WeaponSkeletalMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ATDRPGCharacterBase::BeginPlay()
