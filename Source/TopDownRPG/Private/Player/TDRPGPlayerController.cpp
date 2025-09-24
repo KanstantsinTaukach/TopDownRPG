6a8a66c9c22ba99ff090993cac564e2946ac70b6
Kot <Tovkachkot@gmail.com>
2025-09-24 15:55:58 +0300

    Add Player Controller

--- Source/TopDownRPG/Private/Player/TDRPGPlayerController.cpp
+++ Source/TopDownRPG/Private/Player/TDRPGPlayerController.cpp
@@ -1,3 +1,28 @@
 // Copyright K.Taukach
 
 #include "Player/TDRPGPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+ ATDRPGPlayerController::ATDRPGPlayerController()
+{
+     bReplicates = true;     
+}
+
+void ATDRPGPlayerController::BeginPlay()
+{
+     Super::BeginPlay();
+     
+     check(PlayerMappingContext);
+
+     UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+     check(Subsystem);
+     Subsystem->AddMappingContext(PlayerMappingContext, 0);
+
+     bShowMouseCursor = true;
+     DefaultMouseCursor = EMouseCursor::Default;
+
+     FInputModeGameAndUI InputModeData;
+     InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+     InputModeData.SetHideCursorDuringCapture(false);
+     SetInputMode(InputModeData);
+}
--- Source/TopDownRPG/Public/Player/TDRPGPlayerController.h
+++ Source/TopDownRPG/Public/Player/TDRPGPlayerController.h
@@ -6,9 +6,20 @@
 #include "GameFramework/PlayerController.h"
 #include "TDRPGPlayerController.generated.h"
 
+class UInputMappingContext;
+
 UCLASS()
 class TOPDOWNRPG_API ATDRPGPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+    ATDRPGPlayerController();
+    
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    UPROPERTY(EditAnywhere, Category = "Input")
+    TObjectPtr<UInputMappingContext> PlayerMappingContext;
 };
