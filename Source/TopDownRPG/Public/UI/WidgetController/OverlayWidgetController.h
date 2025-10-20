7941e88e83dff6ad963eea0e9e8ff8a3a65f30bf
Kot <Tovkachkot@gmail.com>
2025-10-20 21:43:43 +0300

    UI Widget Data Table

--- Source/TopDownRPG/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/TopDownRPG/Public/UI/WidgetController/OverlayWidgetController.h
@@ -4,14 +4,34 @@
 
 #include "CoreMinimal.h"
 #include "UI/WidgetController/TDRPGWidgetController.h"
+#include "AbilitySystem/TDRPGAbilitySystemComponent.h"
 #include "OverlayWidgetController.generated.h"
 
+class UTDRPGUserWidget;
 struct FOnAttributeChangeData;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
+
+USTRUCT(BlueprintType)
+struct FUIWidgetRow : public FTableRowBase
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    FGameplayTag MessageTag = FGameplayTag();
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    FText Message = FText();
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    TSubclassOf<UTDRPGUserWidget> MessageWidget;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    TObjectPtr<UTexture2D> Image = nullptr;
+};
 
 UCLASS(Blueprintable, Blueprintable)
 class TOPDOWNRPG_API UOverlayWidgetController : public UTDRPGWidgetController
@@ -33,6 +53,9 @@ public:
     FOnMaxManaChangedSignature OnMaxManaChanged;
 
 protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WidgetData")
+    TObjectPtr<UDataTable> MessageWidgetDataTable;
+    
     void HealthChanged(const FOnAttributeChangeData& Data) const;
     void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
     
