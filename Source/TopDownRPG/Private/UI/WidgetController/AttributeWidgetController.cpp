d156d3475f186fcd204920988fe8d50cd1ea2a19
Kot <Tovkachkot@gmail.com>
2025-11-06 11:59:06 +0300

    Attribute Menu Widget Controller

---
+++ Source/TopDownRPG/Private/UI/WidgetController/AttributeWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright K.Taukach
+
+
+#include "UI/WidgetController/AttributeWidgetController.h"
+
+void UAttributeWidgetController::BroadcastInitialValues()
+{
+    
+}
+
+void UAttributeWidgetController::BindCallbacksToDependencies()
+{
+    
+}
---
+++ Source/TopDownRPG/Public/UI/WidgetController/AttributeWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright K.Taukach
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/TDRPGWidgetController.h"
+#include "AttributeWidgetController.generated.h"
+
+UCLASS()
+class TOPDOWNRPG_API UAttributeWidgetController : public UTDRPGWidgetController
+{
+	GENERATED_BODY()
+
+public:
+    virtual void BroadcastInitialValues() override;
+    virtual void BindCallbacksToDependencies() override;	
+};
