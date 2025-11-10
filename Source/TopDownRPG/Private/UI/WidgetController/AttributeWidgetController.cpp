a6ca410c3af28c4b58e59fb235ecf2ff08ce1b53
Kot <Tovkachkot@gmail.com>
2025-11-10 12:34:59 +0300

    Attribute Info Delegate

--- Source/TopDownRPG/Private/UI/WidgetController/AttributeWidgetController.cpp
+++ Source/TopDownRPG/Private/UI/WidgetController/AttributeWidgetController.cpp
@@ -3,12 +3,22 @@
 
 #include "UI/WidgetController/AttributeWidgetController.h"
 
+#include "TDRPGGameplayTags.h"
+#include "AbilitySystem/TDRPGAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+
 void UAttributeWidgetController::BroadcastInitialValues()
 {
-    
+    UTDRPGAttributeSet* AS = CastChecked<UTDRPGAttributeSet>(AttributeSet);
+
+    check(AttributeInfo);
+
+    FTDRPGAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FTDRPGGameplayTags::Get().Attributes_Primary_Strength);
+    Info.AttributeValue = AS->GetStrength();
+    AttributeInfoDelegate.Broadcast(Info);
 }
 
 void UAttributeWidgetController::BindCallbacksToDependencies()
 {
-    
+
 }
--- Source/TopDownRPG/Public/UI/WidgetController/AttributeWidgetController.h
+++ Source/TopDownRPG/Public/UI/WidgetController/AttributeWidgetController.h
@@ -6,6 +6,10 @@
 #include "UI/WidgetController/TDRPGWidgetController.h"
 #include "AttributeWidgetController.generated.h"
 
+class UAttributeInfo;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FTDRPGAttributeInfo&, Info);
+
 UCLASS(BlueprintType, Blueprintable)
 class TOPDOWNRPG_API UAttributeWidgetController : public UTDRPGWidgetController
 {
@@ -13,5 +17,12 @@ class TOPDOWNRPG_API UAttributeWidgetController : public UTDRPGWidgetController
 
 public:
     virtual void BroadcastInitialValues() override;
-    virtual void BindCallbacksToDependencies() override;	
+    virtual void BindCallbacksToDependencies() override;
+
+    UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+    FAttributeInfoSignature AttributeInfoDelegate;
+    
+protected:
+    UPROPERTY(EditDefaultsOnly)
+    TObjectPtr<UAttributeInfo> AttributeInfo;
 };
