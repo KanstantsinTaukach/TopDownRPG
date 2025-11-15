8323dac4565c170e0c424ba9999b300cfee336a5
Kot <Tovkachkot@gmail.com>
2025-11-15 21:27:15 +0300

    Responding to Attribute Changes

--- Source/TopDownRPG/Private/UI/WidgetController/AttributeWidgetController.cpp
+++ Source/TopDownRPG/Private/UI/WidgetController/AttributeWidgetController.cpp
@@ -2,26 +2,38 @@
 
 
 #include "UI/WidgetController/AttributeWidgetController.h"
-
-#include "TDRPGGameplayTags.h"
 #include "AbilitySystem/TDRPGAttributeSet.h"
 #include "AbilitySystem/Data/AttributeInfo.h"
 
 void UAttributeWidgetController::BroadcastInitialValues()
 {
     UTDRPGAttributeSet* AS = CastChecked<UTDRPGAttributeSet>(AttributeSet);
-
     check(AttributeInfo);
 
     for(auto& Pair: AS->TagsToAttributes)
     {
-        FTDRPGAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(Pair.Key);
-        Info.AttributeValue = Pair.Value().GetNumericValue(AS);
-        AttributeInfoDelegate.Broadcast(Info);
+        BroadcastAttributeInfo(Pair.Key, Pair.Value());
     }
 }
 
 void UAttributeWidgetController::BindCallbacksToDependencies()
 {
+    UTDRPGAttributeSet* AS = CastChecked<UTDRPGAttributeSet>(AttributeSet);
+    check(AttributeInfo);
+    
+    for(auto& Pair: AS->TagsToAttributes)
+    {
+        AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Pair.Value()).AddLambda(
+            [this, Pair](const FOnAttributeChangeData& Data)
+        {
+            BroadcastAttributeInfo(Pair.Key, Pair.Value());
+        });
+    }
+}
 
+void UAttributeWidgetController::BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const
+{
+    FTDRPGAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(AttributeTag);
+    Info.AttributeValue = Attribute.GetNumericValue(AttributeSet);
+    AttributeInfoDelegate.Broadcast(Info);
 }
--- Source/TopDownRPG/Public/UI/WidgetController/AttributeWidgetController.h
+++ Source/TopDownRPG/Public/UI/WidgetController/AttributeWidgetController.h
@@ -6,6 +6,8 @@
 #include "UI/WidgetController/TDRPGWidgetController.h"
 #include "AttributeWidgetController.generated.h"
 
+struct FGameplayAttribute;
+struct FGameplayTag;
 class UAttributeInfo;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FTDRPGAttributeInfo&, Info);
@@ -25,4 +27,7 @@ public:
 protected:
     UPROPERTY(EditDefaultsOnly)
     TObjectPtr<UAttributeInfo> AttributeInfo;
+
+private:
+    void BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const;
 };
