9492d558cfa1c73753f93ebae1cf761627ac760b
Kot <Tovkachkot@gmail.com>
2025-12-24 01:10:49 +0300

    ExecCalcs - capturing attributes

--- Source/TopDownRPG/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/TopDownRPG/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -1,12 +1,28 @@
 // Copyright K.Taukach
 
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
-
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/TDRPGAttributeSet.h"
+
+struct TDRPGDamageStatics
+{
+    DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+    
+    TDRPGDamageStatics()
+    {
+        DEFINE_ATTRIBUTE_CAPTUREDEF(UTDRPGAttributeSet, Armor, Target, false);    
+    }
+};
+
+static const TDRPGDamageStatics DamageStatics()
+{
+    static TDRPGDamageStatics DStatics;
+    return DStatics;
+}
 
 UExecCalc_Damage::UExecCalc_Damage()
 {
-    
+    RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -18,5 +34,20 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
     const AActor* SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
     const AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
 
-    const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();    
+    const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+    const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+    const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+    
+    FAggregatorEvaluateParameters EvaluationParams;
+    EvaluationParams.SourceTags = SourceTags;
+    EvaluationParams.TargetTags = TargetTags;
+    
+    float Armor = 0;
+    ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParams, Armor);
+    Armor = FMath::Max<float>(0.0f, Armor);
+    ++Armor;
+    
+    const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+    OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
