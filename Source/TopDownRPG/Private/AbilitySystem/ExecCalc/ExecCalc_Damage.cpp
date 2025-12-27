a2a94bab3dd9127e86ec867d44b56dedd183dbf8
Kot <Tovkachkot@gmail.com>
2025-12-27 14:02:43 +0300

    Implementing block chance

--- Source/TopDownRPG/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/TopDownRPG/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -2,15 +2,18 @@
 
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 #include "AbilitySystemComponent.h"
+#include "TDRPGGameplayTags.h"
 #include "AbilitySystem/TDRPGAttributeSet.h"
 
 struct TDRPGDamageStatics
 {
     DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+    DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
     
     TDRPGDamageStatics()
     {
-        DEFINE_ATTRIBUTE_CAPTUREDEF(UTDRPGAttributeSet, Armor, Target, false);    
+        DEFINE_ATTRIBUTE_CAPTUREDEF(UTDRPGAttributeSet, Armor, Target, false);
+        DEFINE_ATTRIBUTE_CAPTUREDEF(UTDRPGAttributeSet, BlockChance, Target, false);
     }
 };
 
@@ -23,6 +26,7 @@ static const TDRPGDamageStatics DamageStatics()
 UExecCalc_Damage::UExecCalc_Damage()
 {
     RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+    RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -42,12 +46,20 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
     FAggregatorEvaluateParameters EvaluationParams;
     EvaluationParams.SourceTags = SourceTags;
     EvaluationParams.TargetTags = TargetTags;
+
+    // Get Damage Set by Caller Magnitude
+    float Damage = Spec.GetSetByCallerMagnitude(FTDRPGGameplayTags::Get().Damage);
+
+    // Capture BlockChance on Target, and determine if there was a successful Block
+    // If Block, halve the damage.
+
+    float TargetBlockChance = 0;
+    ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluationParams, TargetBlockChance);
+    TargetBlockChance = FMath::Max<float>(0, TargetBlockChance);
     
-    float Armor = 0;
-    ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParams, Armor);
-    Armor = FMath::Max<float>(0.0f, Armor);
-    ++Armor;
+    const bool bBlocked = FMath::FRandRange(0.0f, 100.0f) < TargetBlockChance;
+    Damage = bBlocked ? Damage / 2.0f : Damage;
     
-    const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+    const FGameplayModifierEvaluatedData EvaluatedData(UTDRPGAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
     OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
