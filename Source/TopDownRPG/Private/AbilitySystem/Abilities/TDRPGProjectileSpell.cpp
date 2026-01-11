7d09b09bafc772df3b0ebe30edc79ab9aa912cb2
Kot <Tovkachkot@gmail.com>
2026-01-11 16:05:41 +0300

    The Gameplay Effect Context

--- Source/TopDownRPG/Private/AbilitySystem/Abilities/TDRPGProjectileSpell.cpp
+++ Source/TopDownRPG/Private/AbilitySystem/Abilities/TDRPGProjectileSpell.cpp
@@ -39,7 +39,17 @@ void UTDRPGProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocat
             ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
 
         const UAbilitySystemComponent* SourceASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
-        const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
+        FGameplayEffectContextHandle EffectContextHandle = SourceASC->MakeEffectContext();
+        EffectContextHandle.SetAbility(this);
+        EffectContextHandle.AddSourceObject(Projectile);
+        TArray<TWeakObjectPtr<AActor>> Actors;
+        Actors.Add(Projectile);
+        EffectContextHandle.AddActors(Actors);
+        FHitResult HitResult;
+        HitResult.Location = ProjectileTargetLocation;
+        EffectContextHandle.AddHitResult(HitResult);
+        
+        const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), EffectContextHandle);
 
         const FTDRPGGameplayTags GameplayTags = FTDRPGGameplayTags::Get();
         const float ScaledDamage = Damage.GetValueAtLevel(GetAbilityLevel());
