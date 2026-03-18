5160d9e3dd1a6d93c8ac1baa721c05d708ba60b1
Kot <Tovkachkot@gmail.com>
2026-03-18 19:45:35 +0300

    Add Montage gameplay tags

--- Source/TopDownRPG/Private/TDRPGGameplayTags.cpp
+++ Source/TopDownRPG/Private/TDRPGGameplayTags.cpp
@@ -96,9 +96,16 @@ void FTDRPGGameplayTags::InitializeNativeGameplayTags()
         FName("Effects.HitReact"), FString("Tag granted when Hit Reacting"));
     
     /*
-    * Abilities
+    * Montage
     */
 
     GameplayTags.Abilities_Attack = UGameplayTagsManager::Get().AddNativeGameplayTag(
         FName("Abilities.Attack"), FString("Attack Ability Tag"));
+
+    GameplayTags.Montage_Attack_Weapon = UGameplayTagsManager::Get().AddNativeGameplayTag(
+            FName("Montage.Attack.Weapon"), FString("Weapon"));
+    GameplayTags.Montage_Attack_RightHand = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Montage.Attack.RightHand"), FString("Right hand"));
+    GameplayTags.Montage_Attack_LeftHand = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Montage.Attack.LeftHand"), FString("Left hand"));
 }
