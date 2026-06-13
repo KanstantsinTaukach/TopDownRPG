7fe480674247e4e3625e8039268f27b922b85c32
Kot <Tovkachkot@gmail.com>
2026-06-13 14:08:46 +0300

    Level up info data asset

---
+++ Source/TopDownRPG/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,34 @@
+// Copyright K.Taukach
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FTDRPGLevelUpInfo
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditDefaultsOnly)
+    int32 LevelUpRequirement = 0;
+    
+    UPROPERTY(EditDefaultsOnly)
+    int32 AttributePointAward = 1;
+
+    UPROPERTY(EditDefaultsOnly)
+    int32 SpellPointAward = 1;
+};
+
+UCLASS()
+class TOPDOWNRPG_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+    UPROPERTY(EditDefaultsOnly, Category = "LevelUpInformation")
+    TArray<FTDRPGLevelUpInfo> LevelUpInformation;
+    
+    int32 FindLevelForXP(int32 XP) const;
+};
