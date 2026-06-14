73ab6cb1508f814e701e0eaec55647e6a0e2571c
Kot <Tovkachkot@gmail.com>
2026-06-14 18:21:16 +0300

    Adding XP to the player state

--- Source/TopDownRPG/Private/Player/TDRPGPlayerState.cpp
+++ Source/TopDownRPG/Private/Player/TDRPGPlayerState.cpp
@@ -21,6 +21,7 @@ void ATDRPGPlayerState::GetLifetimeReplicatedProps(TArray<class FLifetimePropert
     Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
     DOREPLIFETIME(ATDRPGPlayerState, Level);
+    DOREPLIFETIME(ATDRPGPlayerState, XP);
 }
 
 UAbilitySystemComponent* ATDRPGPlayerState::GetAbilitySystemComponent() const
@@ -28,6 +29,36 @@ UAbilitySystemComponent* ATDRPGPlayerState::GetAbilitySystemComponent() const
     return AbilitySystemComponent;
 }
 
+void ATDRPGPlayerState::SetXP(int32 NewXP)
+{
+    XP = NewXP;
+    OnXPChangedDelegate.Broadcast(XP);
+}
+
+void ATDRPGPlayerState::SetLevel(int32 NewLevel)
+{
+    Level = NewLevel;
+    OnLevelChangedDelegate.Broadcast(Level);
+}
+
+void ATDRPGPlayerState::AddToXP(int32 InXP)
+{
+    XP += InXP;
+    OnXPChangedDelegate.Broadcast(XP);
+}
+
+void ATDRPGPlayerState::AddToLevel(int32 InLevel)
+{
+    Level += InLevel;
+    OnLevelChangedDelegate.Broadcast(Level);
+}
+
+void ATDRPGPlayerState::OnRep_XP(int32 OldXP)
+{
+    OnXPChangedDelegate.Broadcast(XP);
+}
+
 void ATDRPGPlayerState::OnRep_Level(int32 OldLevel)
 {
+    OnLevelChangedDelegate.Broadcast(Level);
 }
--- Source/TopDownRPG/Public/Player/TDRPGPlayerState.h
+++ Source/TopDownRPG/Public/Player/TDRPGPlayerState.h
@@ -10,6 +10,8 @@
 class UAbilitySystemComponent;
 class UAttributeSet;
 
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChangedSignature, int32 /*StatValue*/)
+
 UCLASS()
 class TOPDOWNRPG_API ATDRPGPlayerState : public APlayerState, public IAbilitySystemInterface
 {
@@ -18,12 +20,22 @@ class TOPDOWNRPG_API ATDRPGPlayerState : public APlayerState, public IAbilitySys
 public:
 	ATDRPGPlayerState();
 
+    FOnPlayerStatChangedSignature OnXPChangedDelegate;
+    FOnPlayerStatChangedSignature OnLevelChangedDelegate;
+
     virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 
     virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
     UAttributeSet* GetAttributeSet() const { return AttributeSet; };
 
-    FORCEINLINE int32 GetPlayerLevel() const { return Level; };
+    FORCEINLINE int32 GetPlayerLevel() const { return Level; };    
+    FORCEINLINE int32 GetXO() const { return XP; };
+    
+    void SetXP(int32 NewXP);
+    void SetLevel(int32 NewLevel);
+    
+    void AddToXP(int32 InXP);
+    void AddToLevel(int32 InLevel);
     
 protected:
     UPROPERTY(VisibleAnywhere)
@@ -35,7 +47,11 @@ protected:
 private:
     UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_Level)
     int32 Level = 1;
-
+    UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_XP)
+    int32 XP = 0;
+    
     UFUNCTION()
     void OnRep_Level(int32 OldLevel);
+    UFUNCTION()
+    void OnRep_XP(int32 OldXP);
 };
